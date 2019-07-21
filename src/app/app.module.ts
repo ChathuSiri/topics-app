0134fa78929c7ef12b2283965209525004c6ce18
chathuras.12"cse.mrt.ac.lk <Cata"1993>
2019-07-21 12:12:35 +0800

    topics-app: text area with character limit to add topic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { TopicsComponent } from './components/topics/topics.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AddTopicComponent } from './components/add-topic/add-topic.component';
 import {FormsModule} from '@angular/forms';
-import { TopicComponent } from './components/topic/topic.component';
 
 const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 
